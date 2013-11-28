d36c8b2439999a04a43ddda358e411f23e82062f
Ricardo Cruz <piradoiv@gmail.com>
2013-11-28 21:24:24 +0000

    Add validation to user model

--- application/models/user.php
+++ application/models/user.php
@@ -2,6 +2,19 @@
 
 class User extends Datamapper
 {
+  public $validation = array(
+    'email' => array(
+      'field' => 'email',
+      'label' => 'Email',
+      'rules' => array('required', 'valid_email')
+    ),
+    'password' => array(
+      'field' => 'password',
+      'label' => 'Password',
+      'rules' => array('required')
+    )
+  );
+
   public function __construct($id = null)
   {
     parent::__construct($id);
