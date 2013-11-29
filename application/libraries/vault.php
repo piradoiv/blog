01471a88c0ee372188824f6a8621b9f77761d208
Ricardo Cruz <piradoiv@gmail.com>
2013-11-29 14:40:31 +0000

    Fix vault issues

--- application/libraries/vault.php
+++ application/libraries/vault.php
@@ -2,28 +2,30 @@
 
 class Vault
 {
-  private $_ci    = null;
   private $logged = null;
   private $user   = null;
 
   public function __construct()
   {
     $this->watchdog(false);
+    $_ci = &get_instance();
+    $_ci->output->enable_profiler(true);
   }
 
   public function watchdog($redirect = false)
   {
     if ($this->logged === null) {
-      $_ci      = $this->ci();
-      $user     = new $_ci->user;
-      $email    = $this->ci()->session->userdata('email');
-      $password = $this->ci()->session->userdata('password');
+      $_ci      = &get_instance();
+      $user     = new User;
+      $email    = $_ci->session->userdata('email');
+      $password = $_ci->session->userdata('password');
 
       $response = $user->checkLogin($email, $password);
 
       if (!$response) {
         $this->logged = false;
         $this->user   = null;
+        $_ci->session->unset_userdata('password');
       } else {
         $this->logged = true;
         $this->user   = $response;
@@ -39,18 +41,10 @@ class Vault
 
   public function login($email = null, $password = null, $redirect = false)
   {
-    $this->session->set_userdata('email', $email);
-    $this->session->set_userdata('password', $password);
+    $_ci = &get_instance();
+    $_ci->session->set_userdata('email', $email);
+    $_ci->session->set_userdata('password', $password);
     $this->watchdog($redirect);
-  }
-
-  private function ci()
-  {
-    if (!$this->_ci) {
-      $this->_ci = &get_instance();
-    }
-
-    return $this->_ci;
   }
 }
 
