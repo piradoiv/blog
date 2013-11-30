b1dce8e0cdeabb47380a4166e6fdead32905eba7
Ricardo Cruz <piradoiv@gmail.com>
2013-11-30 01:04:31 +0000

    Improve post/show view and post/edit

--- application/views/posts/edit.php
+++ application/views/posts/edit.php
@@ -2,9 +2,18 @@
 <div class="grid-container">
   <div class="grid-50">
     <?= form_open("posts/update/{$post->id}") ?>
-    <?= form_input('title', $post->title) ?>
-    <?= form_input('subtitle', $post->subtitle) ?>
-    <?= form_input('slug', $post->slug) ?>
+    <p>
+      <?= form_label('Title', 'title') ?>
+      <?= form_input('title', $post->title) ?>
+    </p>
+    <p>
+      <?= form_label('Subtitle', 'subtitle') ?>
+      <?= form_input('subtitle', $post->subtitle) ?>
+    </p>
+    <p>
+      <?= form_label(site_url('p').'/', 'slug') ?>
+      <?= form_input('slug', $post->slug) ?>
+    </p>
     <?= form_textarea('contents', $post->contents) ?>
     <?= form_dropdown('published',
       array('no' => 'No', 'yes' => 'Yes'), $post->published)
--- application/views/posts/show.php
+++ application/views/posts/show.php
@@ -1,4 +1,9 @@
 
-<h1><?= $post->title ?></h1>
-<?= $post->render() ?>
+<div class="grid-container">
+  <div class="grid-50 push-25">
+    <h1><?= $post->title ?></h1>
+    <h2><?= $post->subtitle ?></h2>
+    <?= $post->render() ?>
+  </div>
+</div>
 
