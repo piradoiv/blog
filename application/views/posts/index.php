7e0b89502e33d7fe6758465e37e49f7bf1e1ab8c
Ricardo Cruz <piradoiv@gmail.com>
2013-11-30 02:34:31 +0000

    Modify routes and post list

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,11 +1,18 @@
 
-<ul>
-<?php foreach ($posts as $current): ?>
-  <li>
-    <a href="<?= $current->permalink() ?>">
-      #<?= $current->id ?> <?= $current->title ?>
-    </a>
-  </li>
-<?php endforeach ?>
-</ul>
+<div class="grid-container">
+  <div class="grid-50 push-25">
+  <?php foreach ($posts as $current): ?>
+    <div class="post" style="margin-bottom: 25px;">
+      <div class="title">
+        <a href="<?= $current->permalink() ?>">
+          <?= $current->title ?>
+        </a>
+      </div>
+      <div class="subtitle">
+        <?= $current->subtitle ?>
+      </div>
+    </div>
+  <?php endforeach ?>
+  </div>
+</div>
 
