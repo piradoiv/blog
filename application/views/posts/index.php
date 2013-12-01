9faf3146bbbf3938d9542425b1de432a75771f16
Ricardo Cruz <piradoiv@gmail.com>
2013-12-01 19:47:56 +0000

    Add more styles to the app

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -3,11 +3,11 @@
   <div class="grid-50 push-25">
   <?php foreach ($posts as $current): ?>
     <div class="post" style="margin-bottom: 25px;">
-      <div class="title">
+      <h2 class="title">
         <a href="<?= $current->permalink() ?>">
           <?= $current->title ?>
         </a>
-      </div>
+      </h2>
       <div class="subtitle">
         <?= $current->subtitle ?>
       </div>
