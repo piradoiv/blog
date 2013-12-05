d6386e5f1aa3e6f2edff8d49943b8bf218d94496
Ricardo Cruz <piradoiv@gmail.com>
2013-12-05 09:31:55 +0000

    Fix a bug showing the edit button where it shouldn't appear

--- application/views/posts/show.php
+++ application/views/posts/show.php
@@ -3,14 +3,16 @@
   <div class="grid-50 push-25">
     <h1>
       <?= $post->title ?>
-      <?php if ($this->vault->isLogged() &&
-        $post->user_id == $this->vault->user->id): ?>
-      <?php endif ?>
     </h1>
     <h2><?= $post->subtitle ?></h2>
+
+    <?php if ($this->vault->isLogged() &&
+      $post->user_id == $this->vault->user->id): ?>
     <p>
       [ <a href="<?= $post->permalink('edit') ?>">Edit</a> ]
     </p>
+    <?php endif ?>
+
     <?= $post->render() ?>
   </div>
 </div>
