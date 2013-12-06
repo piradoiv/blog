9cc7ad3ebd9a7f4aa8d14771fe8ea991e6f88a17
Ricardo Cruz <piradoiv@gmail.com>
2013-12-06 22:13:01 +0000

    Refactor and Fix multiple CodeMirror

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+$(function()
+{
+  var cmCounter = 0;
+  $('.codemirror').each(function()
+  {
+    $(this).attr('id', 'codemirror-' + cmCounter);
+    useEditorOn($(this)[0]);
+    cmCounter++;
+  });
+
+  function useEditorOn(id)
+  {
+    console.log(id);
+    CodeMirror.fromTextArea(id, {
+      theme: 'monokai',
+      lineNumbers: true,
+      mode: 'application/x-httpd-php'
+    });
+  }
+});
+
