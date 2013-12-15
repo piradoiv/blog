daed2f643e6833524aed4b701bf3c9d1621ca7ee
Ricardo Cruz <piradoiv@gmail.com>
2013-12-15 01:11:00 +0000

    Fixed position for article editor

--- application/views/posts/edit.php
+++ application/views/posts/edit.php
@@ -1,40 +1,43 @@
 
 <div class="grid-container">
-  <div class="grid-50 article-editor">
-    <?= form_open("posts/update/{$post->id}") ?>
-    <p>
-      <?= form_label('Title', 'title') ?>
-      <?= form_input('title', $post->title) ?>
-    </p>
-    <p>
-      <?= form_label('Subtitle', 'subtitle') ?>
-      <?= form_input('subtitle', $post->subtitle) ?>
-    </p>
-    <p>
-      <?= form_label(base_url(), 'slug') ?>
-      <?= form_input('slug', $post->slug) ?>
-    </p>
-    <?= form_textarea('contents', $post->contents) ?>
+  <div class="grid-50">
+    <div class="article-editor">
+      <?= form_open("posts/update/{$post->id}") ?>
+      <p>
+        <?= form_label('Title', 'title') ?>
+        <?= form_input('title', $post->title) ?>
+      </p>
+      <p>
+        <?= form_label('Subtitle', 'subtitle') ?>
+        <?= form_input('subtitle', $post->subtitle) ?>
+      </p>
+      <p>
+        <?= form_label(base_url(), 'slug') ?>
+        <?= form_input('slug', $post->slug) ?>
+      </p>
+      <?= form_textarea('contents', $post->contents) ?>
 
-    <p>
-      <?= form_label('Tags', 'tags') ?>
-      <?= form_input('tags', $post->implodeTags()) ?>
-    </p>
+      <p>
+        <?= form_label('Tags', 'tags') ?>
+        <?= form_input('tags', $post->implodeTags()) ?>
+      </p>
 
-    <p>
-      <?= form_label('Published') ?>
-      <?= form_dropdown('published',
-        array('no' => 'No', 'yes' => 'Yes'), $post->published)
-      ?>
-    </p>
-    <p>
-      <?= form_submit('submit', 'Save') ?>
-      <?php if (isset($post->id)): ?>
-      <a href="<?= $post->permalink() ?>">View post</a>
-      <a href="<?= $post->permalink('delete') ?>" onclick="return confirm('Are you sure?')">Delete</a>
-      <?php endif ?>
-    </p>
-    <?= form_close() ?>
+      <p>
+        <?= form_label('Published') ?>
+        <?= form_dropdown('published',
+          array('no' => 'No', 'yes' => 'Yes'), $post->published)
+        ?>
+      </p>
+      <p>
+        <?= form_submit('submit', 'Save') ?>
+        <?php if (isset($post->id)): ?>
+        <a href="<?= $post->permalink() ?>">View post</a>
+        <a href="<?= $post->permalink('delete') ?>" onclick="return confirm('Are you sure?')">Delete</a>
+        <?php endif ?>
+      </p>
+      <?= form_close() ?>
+    </div>
+    &nbsp;
   </div>
   <div class="grid-50 article-preview">
   </div>
