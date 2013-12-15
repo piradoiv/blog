6b379124cde03a0abb9750ee3700d3a98ca14813
Ricardo Cruz <piradoiv@gmail.com>
2013-12-15 18:40:40 +0000

    Add author information on heading

--- application/views/posts/render.php
+++ application/views/posts/render.php
@@ -4,6 +4,10 @@
     </h1>
     <h2><?= $post->subtitle ?></h2>
 
+    <div class="author-heading">
+      Un artículo de <?= $author->fullName() ?>
+    </div>
+
     <?php if ($this->vault->isLogged() &&
       $post->user_id == $this->vault->user->id): ?>
     <p>
