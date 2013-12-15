343e291f25573382ace02d36f7dbf6140b3570b8
Ricardo Cruz <piradoiv@gmail.com>
2013-12-15 00:36:05 +0000

    Fix template routes

--- application/views/template.php
+++ application/views/template.php
@@ -6,13 +6,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
 <title><?= isset($pageTitle) ? $pageTitle : null ?></title>
 <!--[if lt IE 9]>
-  <script src="<?= base_url() ?>/unsemantic/javascripts/html5.js"></script>
+  <script src="<?= base_url() ?>unsemantic/javascripts/html5.js"></script>
 <![endif]-->
 <!--[if (gt IE 8) | (IEMobile)]><!-->
-  <link rel="stylesheet" href="<?= base_url() ?>/unsemantic/stylesheets/unsemantic-grid-responsive.css" />
+  <link rel="stylesheet" href="<?= base_url() ?>unsemantic/stylesheets/unsemantic-grid-responsive.css" />
 <!--<![endif]-->
 <!--[if (lt IE 9) & (!IEMobile)]>
-  <link rel="stylesheet" href="<?= base_url() ?>/unsemantic/stylesheets/ie.css" />
+  <link rel="stylesheet" href="<?= base_url() ?>unsemantic/stylesheets/ie.css" />
 <![endif]-->
   <link rel="stylesheet" href="<?= base_url() ?>css/app.css" />
   <script src="<?= base_url() ?>codemirror/codemirror.js"></script>
@@ -64,8 +64,8 @@
 
   <?= isset($yield) ? $yield : null ?>
 
-  <script src="<?= base_url() ?>/unsemantic/javascripts/jquery.js"></script>
-  <script src="<?= base_url() ?>/js/app.js"></script>
+  <script src="<?= base_url() ?>unsemantic/javascripts/jquery.js"></script>
+  <script src="<?= base_url() ?>js/app.js"></script>
 </body>
 </html>
 
